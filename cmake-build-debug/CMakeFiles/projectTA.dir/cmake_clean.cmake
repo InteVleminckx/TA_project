0d997861ba4d97592930b2034e5e1ddd749e6f57
InteVleminckx <s0201844@student.uantwerpen.be>
2021-04-28 15:35:23 +0200

    Files

---
+++ cmake-build-debug/CMakeFiles/projectTA.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projectTA.dir/main.cpp.obj"
+  "libprojectTA.dll.a"
+  "projectTA.exe"
+  "projectTA.exe.manifest"
+  "projectTA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectTA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projectTA.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projectTA.
+# This may be replaced when dependencies are built.
