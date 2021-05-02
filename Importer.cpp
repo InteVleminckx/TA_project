94e9f3fcbe89b074f2698502e082262cc6dcdabc
jonasdemaeyer1 <jonas.demaeyer@student.uantwerpen.be>
2021-05-02 12:55:18 +0200

    Importer gemaakt

---
+++ Importer.cpp
@@ -0,0 +1,49 @@
+#include "Importer.h"
+
+/*
+ * Functie die een xml-file gaat uitlezen en opslaan in onze eigen datastructuur
+ * @param filename : naam van de xml-file die we willen uitlezen (type = string)
+ * @return none (voorlopig)
+ * @exceptions none
+ */
+void Importer::readXMLFile(const string &filename) {
+    TiXmlDocument doc;
+
+    // Niet inleesbaar ==> error wegsturen via cerr
+    if (!doc.LoadFile(filename.c_str())) {
+        cerr << doc.ErrorDesc() << endl;
+    }
+
+    // Root eruithalen (is element met naam bestemmingen)
+    TiXmlElement* root = doc.FirstChildElement();
+
+    // Checken of de root bestaat
+    if (root == NULL) {
+        cerr << "Failed to load file: No root element." << endl;
+        doc.Clear();
+    }
+
+    // Checken of naam van root "bestemmingen" is
+    string rootName = root->Value();
+    if (rootName != "bestemmingen") {
+        cerr << "Hoofdtag is niet bestemmingen" << endl;
+        doc.Clear();
+    }
+
+    // Loopen over alle childtags
+    for (TiXmlElement* elem = root->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement()) {
+
+        // Checken of childtag "halte" heet
+        string elemName = elem->Value();
+        if (elemName != "halte") {
+            cerr << "Naam van childtag is niet halte" << endl;
+        }
+
+        // Info tussen twee tags halen
+        else {
+            string info = elem->FirstChild()->ToText()->Value();
+            cout << info << endl;
+            // TODO: deze info opslagen in eigen datastructuur
+        }
+    }
+}
---
+++ Importer.h
@@ -0,0 +1,24 @@
+/*
+ * Dit is de importerklasse. Het heeft als voornamelijke functie het uitlezen van xml-bestanden en deze in onze
+ * eigen gemaakte datastructuur steken.
+ * @date: 02/05/2021
+ * @version: 1.0
+ */
+
+#ifndef PROJECTTA_IMPORTER_H
+#define PROJECTTA_IMPORTER_H
+
+#include "txml/tinystr.h"
+#include "txml/tinyxml.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Importer {
+public:
+    static void readXMLFile(const string &filename);
+};
+
+
+#endif //PROJECTTA_IMPORTER_H
