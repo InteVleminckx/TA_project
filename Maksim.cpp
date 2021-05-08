f28bc3f45cf45491edb21dca16b43ec8f47e3377
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2021-05-08 22:45:00 +0200

    functie generateRE() in klasse Maksim verder geschreven 8/05

--- Maksim.cpp
+++ Maksim.cpp
@@ -4,6 +4,9 @@
 
 #include "Maksim.h"
 
+//    Maksim maksim;
+//    string a = "010100";
+//    maksim.stringToBarcode(a);
 
 void Maksim::stringToBarcode(string &str) {
 
@@ -30,7 +33,7 @@ void Maksim::stringToBarcode(string &str) {
         fIniFile << "rotateX = 0" << endl;
         fIniFile << "rotateY = 0" << endl;
         fIniFile << "rotateZ = 0" << endl;
-        fIniFile << "center = " << "(0, " << i*0.5 << ", 0)" << endl;
+        fIniFile << "center = " << "(0, " << i*0.5 << ", 0)" << endl; // de y verandert telkens met
         if (str[i] == '1') {
             fIniFile << "color = " << "(0, 0, 0)" << endl;
         }
@@ -43,9 +46,61 @@ void Maksim::stringToBarcode(string &str) {
     fIniFile.close();
 }
 
-string Maksim::generateRE() {
+// Maksim maksim;
+//    srand ((int)time(NULL));
+//    for (auto i = 0; i < 10; i++) {
+//        string re = maksim.generateRE();
+//        cout << re << endl;
+//    }
+string Maksim::generateRE(int numberOfIterations) { // het aantal iteraties bepaalt hoeveel bewerkingen we gaan uitvoeren (per deelbewerking)
 
-    string RE = "";
+
+    // het alfabet van de RE is {0,1}
+    vector<string> alfabet{"0", "1"};
+    vector<string> bewerkingen{"unie", "concatenatie", "kleeneStar"};
+
+    string RE;
+
+    string R;
+    string S;
+    string U;
+    string T;
+
+    // R bepalen
+    int symbool1 = rand() % 2;
+    int randomBewerking = rand() % 3;
+    string random;
+    if (randomBewerking == 0) { // unie
+        random = "(" + to_string(symbool1) + "+" + to_string(1-symbool1) + ")";
+    }
+    else if (randomBewerking == 1) { // concat
+        random = "(" + to_string(symbool1) + to_string(1-symbool1) + ")"; // nog maken dat je kan kiezen tussen aa en ab/ba
+    }
+    else if (randomBewerking == 2) { // kleene
+        random = "(" + to_string(symbool1) + ")*";
+    }
+    R = random;
+    // S bepalen
+
+    S = "0";
+    // U bepalen
+
+    U = "0";
+    // T bepalen
+
+    T = "0";
+
+    // formule samenstellen
+    string formule = "(" + R + " + " + S + U + "*" + T + ")" + "*" + S + U + "*"; // algemene vorm van formule
+
+    // doorsnede checken => controlesysteem
+    bool doorsnede = false;
+    if (doorsnede) {
+        return generateRE(numberOfIterations+1);
+    }
+    else {
+        RE = formule;
+    }
 
     return RE;
 }
