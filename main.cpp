c34063bceb65794d0069f295369ff9da1ab1ee64
Maksim Karnaukh <s0201604@ua.ad.ac.be>
2021-05-16 20:25:35 +0200

    cmakelists engine/engine.h toegevoegd

--- main.cpp
+++ main.cpp
@@ -18,23 +18,31 @@
 
 int main() {
 
-    vector<long> times;
-    DFA dfa1 = DFA("../TestenBrzozowski/Test4.json");
-    Brzozowski::brzozowskiAlgorithm(dfa1, times);
-    dfa1.print(cout);
+//    vector<long> times;
+//    DFA dfa1 = DFA("../TestenBrzozowski/Test4.json");
+//    Brzozowski::brzozowskiAlgorithm(dfa1, times);
+//    dfa1.print(cout);
+//
+//    TFA temp_tfa("../TestenBrzozowski/Test4.json");
+//    TFA minimized_TFA = temp_tfa.minimize(times);
+//    minimized_TFA.print(cout);
+//
+//    dfa1.getMemory(times);
+//    DFA dfa2 = minimized_TFA.toDFA();
+//    dfa2.getMemory(times);
+//
+//    cout << "Tijd met Brzozowski ==> " << times[0] << " microseconden" << endl;
+//    cout << "Tijd met TFA ==> " << times[1] << " microseconden" << endl;
+//    cout << "Geheugen DFA met Brzozowski ==> " << times[2] << " bytes" << endl;
+//    cout << "Geheugen DFA met TFA ==> " << times[3] << " bytes" << endl;
 
-    TFA temp_tfa("../TestenBrzozowski/Test4.json");
-    TFA minimized_TFA = temp_tfa.minimize(times);
-    minimized_TFA.print(cout);
-
-    dfa1.getMemory(times);
-    DFA dfa2 = minimized_TFA.toDFA();
-    dfa2.getMemory(times);
-
-    cout << "Tijd met Brzozowski ==> " << times[0] << " microseconden" << endl;
-    cout << "Tijd met TFA ==> " << times[1] << " microseconden" << endl;
-    cout << "Geheugen DFA met Brzozowski ==> " << times[2] << " bytes" << endl;
-    cout << "Geheugen DFA met TFA ==> " << times[3] << " bytes" << endl;
+    Maksim maksim;
+    srand ((int)time(NULL));
+    for (auto i = 0; i < 10; i++) {
+        string re = maksim.generateRE(2);
+        cout << re << endl;
+        cout << endl;
+    }
 
     return 0;
 }
